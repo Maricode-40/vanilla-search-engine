4ec509c17036511b30de817d291f4a776cb948e7
Isabel Rico <marissa.rico28@gmail.com>
2024-02-16 16:03:29 +0100

    fixing code js query selector and api axios

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
-function showTemperature(response) {
-  console.log(response.data.city);
-  console.log(response.data.temperature);
-}
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Sydney&key=46f53o0114a0et1fb464bc0d734f573e";
-
-axios.get(apiUrl).then(showTemperature);
-
 /**
- * event my object to change input
+ * event the object to change input
  */
 
 function search(event) {
@@ -27,3 +18,30 @@ function search(event) {
 
 let form = document.querySelector("#searchTab");
 form.addEventListener("submit", search);
+
+/**
+ * calling api axios
+ */
+
+let city = "Sydney";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=${city}&key=46f53o0114a0et1fb464bc0d734f573e";
+
+function showTemperature(response) {
+  console.log(response);
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  console.log(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+
+  console.log(response.data.city);
+}
+
+axios.get(apiUrl).then(showTemperature);
+
+/**
+ * method  need to returns a collection of elements with the given name.
+ * Then,.... event listener function on click will log-"click" to the console &
+ *  call the function - go UP to keep fixing code
+ */

a5404b8eaeec361783ad60604fe56e419c4fee7a
Isabel Rico <marissa.rico28@gmail.com>
2024-02-17 14:30:21 +0100

    fixing temperature search engine

--- js/main.js
+++ js/main.js
@@ -25,17 +25,17 @@ form.addEventListener("submit", search);
 
 let city = "Sydney";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=${city}&key=46f53o0114a0et1fb464bc0d734f573e";
+  "https://api.shecodes.io/weather/v1/current?query=${city}&key=46f53o0114a0et1fb464bc0d734f573e&units=metric";
 
 function showTemperature(response) {
-  console.log(response);
-  console.log(response.data);
+  //console.log(response);
+  //console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  console.log(response.data.temperature.current);
+  //console.log(response.data.temperature.current);
   temperatureElement.innerHTML = temperature;
 
-  console.log(response.data.city);
+  //console.log(response.data.city);
 }
 
 axios.get(apiUrl).then(showTemperature);

62970b647503dc8102bea1a32a2c449a75cbcf7d
Isabel Rico <marissa.rico28@gmail.com>
2024-02-20 16:52:03 +0100

    fixing decimal using Mathround

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ function search(event) {
   let h1 = document.querySelector("h1");
 
   let city = searchInput.value;
+  /**
+   * calling api axios
+   */
   if (city) {
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=46f53o0114a0et1fb464bc0d734f573e&units=metric`;
 
@@ -26,7 +29,7 @@ function search(event) {
 function showTemperature(response) {
   //console.log(response);
   //console.log(response.data);
-  let temperature = response.data.temperature.current;
+  let temperature = Math.round(response.data.temperature.current);
   //console.log(response.data.temperature.current);
   let cityElement = document.querySelector("#temperature");
   cityElement.innerHTML = temperature;
@@ -37,11 +40,7 @@ let form = document.querySelector("#searchTab");
 form.addEventListener("submit", search);
 
 /**
- * calling api axios
- */
-
-/**
  * method  need to returns a collection of elements with the given name.
  * Then,.... event listener function on click will log-"click" to the console &
- *  call the function - go UP to keep fixing code
+ *  call the function
  */
